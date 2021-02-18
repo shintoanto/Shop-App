ca19d3f9f930786859cb77737ff0b837a2545bfa
shinto <shintopa@outlook.com>
2021-02-18 19:34:04 +0530

    Working on the "Products" Grid & Item Widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Shop',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      home: ProductsOverViewScreen(),
     );
   }
 }
