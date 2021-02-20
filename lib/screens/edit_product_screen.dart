bb9c694a8b523d63f00a1a53da33d5d57048be88
shinto <shintopa@outlook.com>
2021-02-20 10:38:31 +0530

    Using Forms & Working with Form Inputs.

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class EditeProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+
   @override
   _EditeProductScreenState createState() => _EditeProductScreenState();
 }
@@ -8,6 +10,17 @@ class EditeProductScreen extends StatefulWidget {
 class _EditeProductScreenState extends State<EditeProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Product"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+            child: ListView(
+          children: <Widget>[TextFormField()],
+        )),
+      ),
+    );
   }
 }
--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/user_product_item.dart';
 
@@ -15,7 +16,11 @@ class UserProductScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your producst'),
         actions: <Widget>[
-          IconButton(icon: const Icon(Icons.add), onPressed: () {})
+          IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditeProductScreen.routeName);
+              })
         ],
       ),
       drawer: AppDrawer(),
