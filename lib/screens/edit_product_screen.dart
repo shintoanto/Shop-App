c8eb65a6d6827db093d35e75d250a7718875c5e5
shinto <shintopa@outlook.com>
2021-02-20 10:21:02 +0530

    "Edit Product" Screen & A Problem.

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditeProductScreen extends StatefulWidget {
+  @override
+  _EditeProductScreenState createState() => _EditeProductScreenState();
+}
+
+class _EditeProductScreenState extends State<EditeProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
